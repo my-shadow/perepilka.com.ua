a1303813785fc70d41cba2944dc60660a37e4f94
chebotarArt <chebotar.art@gmail.com>
2026-05-28 12:40:02 +0300

    removed screeens, added GTM

--- index.php
+++ index.php
@@ -23,6 +23,15 @@ $_m = $_s['meta'];
             : '';
     ?>
     <?php if ($_siteUrl): ?>
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WQ6QFVMG');</script>
+        <!-- End Google Tag Manager -->
+
     <link rel="canonical" href="<?= htmlspecialchars($_siteUrl) ?>">
     <?php endif; ?>
 
@@ -59,6 +68,10 @@ $_m = $_s['meta'];
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQ6QFVMG"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
     <!-- Header -->
     <header class="header" id="header">
